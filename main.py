913f5a217dedb18463a198917f1a3eb23f869c6c
Sakramentos <kogm818@gmail.com>
2025-01-07 23:34:46 +0300

    реализован REPL, с базовым функционалом калькулятора

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-name = input("Введите имя:")
-print("Hello world!" + name + "!")g
+lst = []
+while True:
+    a = int(input())
+    b = int(input())
+    c = input()
+    if c == '-':
+        d = a-b
+    if c == '+':
+        d = a + b
+    if c == '*':
+        d = a*b
+    if c == '/':
+        d = a/b
+    #f = (a + c + b + "=" + d)
+    f = f"{a} {c} {b} = {d}"
+    lst.append(f)
+    print(lst)
